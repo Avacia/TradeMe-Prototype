84cf9a6dddfe64137976d3882c040cf92b8b1a83
Avacia <yenw@missionreadyhq.com>
2024-08-31 10:40:57 +1200

    update routes and created get and post request in controller for each routes

--- Backend/controllers/sportApplianceController.js
+++ Backend/controllers/sportApplianceController.js
@@ -9,10 +9,35 @@ connectToDb((err) => {
 })
 
 module.exports.updateSportAppliance = async (req, res) => {
-    db.collection("sport appliance")
+    const appliance = req.body
+
+    try{
+        db.collection("sport appliance")
+            .insertOne(appliance)
+            .then(result => {
+                res.status(201).json(result)
+            })
+            .catch(error){
+                res.status(500).json({error: "Could not create new profile or update current profile"})
+            }
+    }
+    catch(error){
+        res.status(500).json({error: "Unable to connect database"})
+    }
 }
 
 module.exports.getSportAppliance = async (req, res) => {
-    db.collection("sport appliance")
-        .find()
+    let sportApplianceArray = []
+
+    try{
+        db.collection("sport appliance")
+            .find()
+            .forEach((appliance) => sportApplianceArray.push(appliance))
+            .then(() =>{
+                res.status(200).json(sportApplianceArray)
+            })
+    }
+    catch(error){
+        res.status(500).json({error: 'Could not fetch the documents'})
+    }
 }
--- Backend/routes/sportApplianceRoutes.js
+++ Backend/routes/sportApplianceRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const sportApplianceController = require('../../controllers/sportApplianceController')
+const sportApplianceController = require('../controllers/sportApplianceController')
 
 router.post("/updateSportAppliance", sportApplianceController.updateSportAppliance)
 router.get("/getSportAppliance", sportApplianceController.getSportAppliance)
